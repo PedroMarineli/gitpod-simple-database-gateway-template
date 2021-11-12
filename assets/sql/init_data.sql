16601c17c77517e44fef841775c96bb2dbcc8030
PedroMarineli <phmo.1606@gmail.com>
2021-11-12 11:33:38 +0000

    criacao banco de dados

--- assets/sql/init_data.sql
+++ assets/sql/init_data.sql
@@ -1,1 +1,7 @@
 -- Código SQL com a inclusão dos registros iniciais do banco de dados
+
+INSERT INTO filmes(titulo) VALUES
+       ('Entre Facas e Segredos'),
+       ('O Hobbit'),
+       ('Guia de Mochileiro das Galáxias'),
+       ('Monty Python: Em busca do Cálice Sagrado'); 
